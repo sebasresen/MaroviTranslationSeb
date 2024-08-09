81d5d318e72e73eee199890b821726d202647baf
sebasresen <sebastian.es.en@gmail.com>
2024-08-08 20:55:19 -0400

    Added special characters, complex expressions and empty input tests.

--- MaroviTranslation/tests/translation_tester.py
+++ MaroviTranslation/tests/translation_tester.py
@@ -18,7 +18,7 @@ class TestCore(unittest.TestCase):
 
 class TestGoogleTranslation(unittest.TestCase):
     @patch('googletrans.Translator')
-    def basic_test(self, MockGTrans):
+    def test_basic_exp(self, MockGTrans):
         mock_trans = MockGTrans.return_value
         mock_trans.translate.return_value.text = "Hola"
 
@@ -27,6 +27,28 @@ class TestGoogleTranslation(unittest.TestCase):
 
         self.assertEqual(result, "Hola")
         mock_trans.translate.assert_called_once_with("Hello", src='en', dest='es')
+
+    @patch('googletrans.Translator')
+    def test_expression(self, MockGTrans):
+        mock_trans = MockGTrans.return_value
+        mock_trans.translate.return_value.text = "¡Buen día!"\
+        
+        gTrans = GoogleTranslator()
+        result = gTrans.translate("Good morning!")
+
+        self.assertEqual(result, "¡Buen día!")
+        mock_trans.translate.assert_called_once_with("Good morning!", src='en', dest='es')
+
+    @patch('googletrans.Translator')
+    def test_empty(self, mockGTrans):
+        mock_trans = mockGTrans.return_value
+        mock_trans.translate.return_value.text = ""
+
+        gTrans = GoogleTranslator()
+        result = gTrans.translate("")
+
+        self.assertEqual(result, "")
+        mock_trans.translate.assert_called_once_with("", src='en', dest='es')
 
 
 if __name__ == "__main__":
